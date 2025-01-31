938144e8b658819da496ffda46b3cc58d2f2c08c
ninad patil <ninad.patil198@gmail.com>
2025-01-31 18:42:16 +0530

    added hello message

--- src/server.js
+++ src/server.js
@@ -9,6 +9,9 @@ const app = express();
 
 app.use(express.json());
 setupSwagger(app);
+app.use("/", (req, res) =>
+  res.status(200).json("Hello there, please go to /api-docs")
+);
 app.use("/auth", authRoutes);
 app.use("/gadgets", gadgetRoutes);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
